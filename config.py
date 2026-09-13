c253cb7d7b6178baacc7b399642e1a8c58d0e10c
asror2727 <asrorake001@gmail.com>
2026-09-13 17:22:44 +0500

    Load environment variables for bot configuration

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8218949042:AAGrMoAgIrHEddRIAP_q-635Z8f1yjfdaA8")
-OWNER_ID = 7651404790
+BOT_USERNAME = os.getenv("BOT_USERNAME", "genzomafiabot")  # @ belgisiz yozing
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7651404790"))  # Telegram ID ingiz
